3f0d6e8de588b0ec892aeeafe5e7af013bf2bc24
Kevin Perez <kevindperezm@gmail.com>
2014-05-07 10:40:55 -0500

    Retirado botón "Imprimir" ya que la característica no está terminada.

--- application/views/catalogo/carrito-confirmado.php
+++ application/views/catalogo/carrito-confirmado.php
@@ -3,8 +3,7 @@
 		<div class='alert alert-dismissable alert-success'>
 			<button class='close' data-dismiss='alert'>x</button>
 			Su compra ha sido registrada. Los detalles de su pedido se 
-			muestran abajo. Si desea imprimir esta página, haga clic en el 
-			botón <b>Imprimir</b>.
+			muestran abajo.
 		</div>
 	</div>
 </div>
@@ -13,7 +12,6 @@
 	<div class='col-xs-12'>
 		<!-- <div class='well'> -->
 			<a class='btn btn-primary' href='<?=link_pagina($this, 'catalogo', 'catalogo')?>'>Volver al catálogo</a>
-			<a href='<?=base_url()?>carrito/imprimir/<?=$pedido->id?>' class='btn btn-default pull-right'>Imprimir</a>
 		<!-- </div> -->
 	</div>
 </div>	
