4c11227e02f8741e0c93cd373de5f76279f7934b
Kevin Perez <kevindperezm@gmail.com>
2014-05-08 00:18:52 -0500

    Eliminar archivos innecesarios y modificar entorno.
    Se eliminaron algunos archivos no necesarios, como los viejos logs de CodeIgniter y se cambió el entorno de CodeIgniter de 'development' a 'production'.

--- application/logs/log-2014-04-30.php
+++
@@ -1,3 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
-
-ERROR - 2014-04-30 15:07:13 --> Severity: Notice  --> Undefined index: HTTP_X_FORWARDED_FOR /home/kevin/Proyectos/carritocompra/application/helpers/login_helper.php 68
--- application/logs/log-2014-05-04.php
+++
@@ -1,3 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
-
-ERROR - 2014-05-04 23:02:04 --> 404 Page Not Found --> acerca-de
