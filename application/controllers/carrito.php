6249a817ca5aa5fe0154c9be7e2f0e77ae28fd7c
Kevin Perez <kevindperezm@gmail.com>
2014-05-15 09:30:51 -0500

    Implementado Strategy en la generación de pdf de Pedido2

--- application/controllers/carrito.php
+++ application/controllers/carrito.php
@@ -228,36 +228,7 @@ class Carrito extends CI_Controller {
 		if (!is_null($data['pedido'])) {
 			# Generar PDF
 			$pedido = $data['pedido'];
-
-			$no = 0;
-			$compras = array();
-			foreach ($pedido->compras as $compra) {
-				$compra_array = array();
-				$compra_array['no.'] = ++$no;
-				$compra_array['imagen'] = base_url().$compra->producto->imagen;
-				$compra_array['codigo'] = $compra->producto->codigo;
-				$compra_array['descripcion'] = $compra->producto->descripcion;
-				$compra_array['variante'] = 'Ninguna';
-				$compra_array['precio_unitario'] = '$'.number_format($compra->precio_unitario, 2);
-				$compra_array['cantidad'] = $compra->cantidad_total;
-				$compra_array['subtotal'] = '$'.number_format($compra->precio_bruto_total, 2);
-				$compras[] = $compra_array;
-			}
-
-			$pdf_data = array(
-				'usuario_nombre' => $pedido->usuario->nombres.' '.$pedido->usuario->apellido_paterno.' '.$pedido->usuario->apellido_materno,
-				'usuario_departamento' => $pedido->usuario->departamento,
-				'usuario_encargado' => $pedido->usuario->encargado_departamento,
-				'pedido_fecha' => $pedido->created_at->format('d/m/Y'),
-				'pedido_hora' => $pedido->created_at->format('H:i'),
-				'compras' => $compras,
-				'nombre_tienda' => NOMBRE_TIENDA
-			);
-
-			$this->load->helper( 'pdf_helper' );
-			$pdf = new Pedido_PDF();
-			$pdf->set_data($pdf_data);
-			$pdf->generate_and_show();	
+			$this->load->view('catalogo/pedido_pdf', $data);	
 			
 		} else {
 			# No existe pedido. Notificar.
