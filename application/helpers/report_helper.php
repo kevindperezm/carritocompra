fa0213e189d7f639efff8dcbd10416b02566052e
Kevin Perez <kevindperezm@gmail.com>
2014-05-15 09:30:51 -0500

    Implementado Strategy en la generación de pdf de Pedido2

--- application/helpers/report_helper.php
+++ application/helpers/report_helper.php
@@ -1,53 +1,59 @@
 <?php
 
-abstract class PDF_Generator {
-	protected $data = array();
-	abstract public function generate_and_show();
+class Report {
+	private $data = array();
+	private $generator;
+	public function Report($generator) { $this->generator = $generator; }
+	public function generate() { $this->generator->generate($this->data); }
 	public function set_data($data) { $this->data = $data; }
 	public function get_data() { return $this->data; }
+	public function set_generator($generator) { $this->generator = $generator; }
+	public function get_generator() { return $this->generator; }
 }
 
-class FPDF_Generator extends PDF_Generator {
+class Generic_Generator {
+	protected function init($data) { return null; }
+	protected function header($data) { return null; }
+	protected function body($data) { return null; }
+	protected function footer($data) { return null; }
+	protected function output($data) { return null; }
+	public function generate($data) {
+		$this->init($data);
+		$this->header($data);
+		$this->body($data);
+		$this->footer($data);
+		$this->output($data);
+	}
+}
+
+class GenericPDF_Generator extends Generic_Generator {
 	private $FPDF_INC = 'third_party/fpdf17/fpdf.php';
 	protected $pdf;
-
-	private function init_pdf() {
+	protected function init($data) {
 		require_once APPPATH.$this->FPDF_INC;
 		$this->pdf = new FPDF();
 		$this->pdf->cMargin = 1;
 		$this->pdf->AddPage();
 	}
-
-	protected function generate_pdf() { return null; }
-
-	private function output_pdf() {
-		$this->pdf->Output();
-	}
-
-	#Override
-	public function generate_and_show() {
-		$this->init_pdf();
-		$this->generate_pdf();
-		$this->output_pdf();
-	}
+	protected function output($data) {	$this->pdf->Output(); }
 }
 
-class Pedido_PDF extends FPDF_Generator {
+class PedidoPDF_Generator extends GenericPDF_Generator {
 	private $encabezados_usuario = array('Usuario', 'Departamento', 'Encargado', 'Fecha', 'Hora');
 	private $encabezados_pedido = array('No.', 'Imagen', 'Código', 'Descripción', 'Variante', 'Precio unitario', 'Cantidad', 'Subtotal');
 	private $columns_width = array(10, 24, 24, 32, 25, 30, 20, 25);
 
-	private function create_header() {
+	private function create_header($data) {
 		$this->pdf->SetFont('Arial', 'B', 18);
 		$x = $this->pdf->GetX();
 		$y = $this->pdf->GetY();
-		$this->pdf->MultiCell(80, 20, $this->data['nombre_tienda']);
+		$this->pdf->MultiCell(80, 20, $data['nombre_tienda']);
 		$this->pdf->SetXY($x + 80, $y);
 		$this->pdf->SetFont('Arial', 'B', 15);
 		$this->pdf->Cell(100, 20, "Detalle de pedido", false, true, 'R');
 	}
 
-	private function create_usuario_details_table() {
+	private function create_usuario_details_table($data) {
 		# Encabezados
 		$this->pdf->SetFont('Arial', 'B', 8.7);
 		$this->pdf->SetFillColor(249, 249, 249);
@@ -60,11 +66,11 @@ class Pedido_PDF extends FPDF_Generator {
 		$this->pdf->SetFillColor(255, 255, 255);
 		$this->pdf->SetDrawColor(221, 221, 221);
 		$columnas = array( 
-			$this->data['usuario_nombre'],
-			$this->data['usuario_departamento'],
-			$this->data['usuario_encargado'],
-			$this->data['pedido_fecha'],
-			$this->data['pedido_hora']
+			$data['usuario_nombre'],
+			$data['usuario_departamento'],
+			$data['usuario_encargado'],
+			$data['pedido_fecha'],
+			$data['pedido_hora']
 		);
 		$this->pdf->SetFont('Arial', '', 8.7);
 		$y = $this->pdf->GetY();
@@ -76,24 +82,21 @@ class Pedido_PDF extends FPDF_Generator {
 		$this->pdf->Ln();
 	}
 
-	private function create_disclaimer() {
+	private function create_disclaimer($data) {
 		$this->pdf->Ln();
-
 		$y = $this->pdf->GetY();
 		$this->pdf->SetFont('Arial', 'B', 7.5);
 		$this->pdf->MultiCell(19, 4, 'Nota: ');
-
 		$this->pdf->SetXY(19, $y);
 		$this->pdf->SetFont('Arial', '', 7.5);
 		$texto = 
 		"Los precios listados en la siguiente tabla son los precios que estaban vigentes en el momento en que este pedido fue elaborado. 
 Los totales mostrados son calculados usando esos precios. Podrían diferir de los precios actuales de cada producto.";
 		$this->pdf->MultiCell(0, 4, utf8_decode($texto), false, 'L');
-
 		$this->pdf->Ln();
 	}
 
-	private function create_pedido_details_table() {
+	private function create_pedido_details_table($data) {
 		# Encabezados
 		$this->pdf->SetFont('Arial', 'B', 8.7);
 		$this->pdf->SetFillColor(249, 249, 249);
@@ -104,19 +107,15 @@ Los totales mostrados son calculados usando esos precios. Podrían diferir de lo
 			$this->pdf->Cell($width, 7, $encabezado, true, false, 'L', true);	
 		}
 		$this->pdf->Ln();
-
 		# Filas
 		$this->pdf->SetFont('Arial', '', 8.7);
 		$this->pdf->SetFillColor(255, 255, 255);
-		
-		foreach ($this->data['compras'] as $compra) {
+		foreach ($data['compras'] as $compra) {
 			$imgsize = getimagesize($compra['imagen']);
 			$height = $imgsize[1] * $width / $imgsize[0] - 0.2;
-
 			for ($i=0; $i < sizeof($compra); $i++) {
 				$width = $i < sizeof($this->columns_width) ? $this->columns_width[$i] : 20;
 				$columna = utf8_decode(array_values($compra)[$i]);
-
 				switch ($i) {
 				case 1:	/* Mostrar imagen */
 					$x = $this->pdf->GetX();
@@ -128,20 +127,27 @@ Los totales mostrados son calculados usando esos precios. Podrían diferir de lo
 					$this->pdf->MultiCell($width, $height, $columna, true);
 					$this->pdf->SetXY($x + $width, $y);
 				}
-
 			}
-
 			$this->pdf->Ln();
 		}
 		$this->pdf->Ln();
 	}
 
-	# Override
-	protected function generate_pdf() {
-		$this->create_header();
-		$this->create_usuario_details_table();
-		$this->create_disclaimer();
-		$this->create_pedido_details_table();
+	#Override
+	protected function header($data) {
+		$this->create_header($data);
+	}
+
+	#Override
+	protected function body($data) {
+		$this->create_usuario_details_table($data);
+		$this->create_disclaimer($data);
+		$this->create_pedido_details_table($data);
+	}
+
+	#Override
+	protected function output($data) {
+		$this->pdf->Output('detalles-pedido'.$data['pedido_fecha'], 'I');
 	}
 }
 
---
+++ application/views/catalogo/pedido_pdf.php
@@ -0,0 +1,2 @@
+<?php $this->load->helper('report_functions_helper') ?>
+<?php pedido_generate_pdf($this, $pedido) ?>
