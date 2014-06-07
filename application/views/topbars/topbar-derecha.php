630fbf261fb5070e03f1310cf71f39d1565177ed
Kevin Perez <kevindperezm@gmail.com>
2014-06-06 21:38:15 -0500

    Agregados tooltips de ayuda en algunos links

--- application/views/topbars/topbar-derecha.php
+++ application/views/topbars/topbar-derecha.php
@@ -1,6 +1,7 @@
 <ul class='nav navbar-nav navbar-right'>
 	<li>
-		<a href='<?=base_url()?>carrito'>
+		<a href='<?=base_url()?>carrito' 
+		 title="Haga clic aquí para ver su carrito de compra y confirmar su venta">
 			<i class='glyphicon glyphicon-shopping-cart'></i> Carrito
 			<?=$this->load->helper('carrito_helper'); boton_carrito($this);?>
 		</a>
@@ -9,5 +10,9 @@
 		<i class='glyphicon glyphicon-user'></i> 
 		<?=$this->session->userdata('user_nombre')?>
 	</p>
-	<li><a href="<?=base_url()?>logout">Salir</a></li>
+	<li>
+		<a href="<?=base_url()?>logout" title="Haga clic aquí para cerrar sesión">
+			Salir
+		</a>
+	</li>
 </ul>
