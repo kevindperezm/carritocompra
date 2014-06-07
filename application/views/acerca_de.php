1a49a9de3a42b5d27d520ba828c0c175f9b107c1
Dannypein <danieldolores117@gmail.com>
2014-06-06 21:42:27 -0500

    empece a llenar acerca de

--- application/views/acerca_de.php
+++ application/views/acerca_de.php
@@ -1,1 +1,20 @@
-<h1>Acerca de</h1> 
+<div class="col-md-12 col-sm-12 col-xs-12">
+<h1>Acerca de</h1>
+<br>
+<h3>Sitio web desarrollada por Mundo Virtual</h3>
+<br>
+<p>Director de la empresa<b>  Hector Manuel Dolores Campos</b></p>
+<br>
+<p>Desarrolladores</p>
+<ul>
+	<li><a>Kevin Dilan Pérez Maulión</a></li>
+	<li><a>Daniel Dolores Cárdenas</a></li>
+<ul>
+<br>
+<h4>Contactenos a los correos</h4>
+	<ul>
+		<li><a>laredmzo@hotmail.com</a></li>
+		<li><a>kevindperezm@gmail.com</a></li>
+		<li><a>danieldolores117@gmail.com</a></li>
+	</ul>
+</div>
