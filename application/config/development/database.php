f1af0cdf6ba2630be607cbdcfdcc8f80d4a63bcf
Kevin Perez <kevindperezm@gmail.com>
2015-05-22 12:11:34 -0500

    añadir configuración para bd de desarrollo
    La BD de desarrollo es una BD Sqlite3.

---
+++ application/config/development/database.php
@@ -0,0 +1,13 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$active_group = 'development';
+$active_record = FALSE;
+
+$db['development']['dbdriver'] = 'sqlite';
+$db['development']['hostname'] = 'database.sqlite';
+$db['development']['database'] = '';
+$db['development']['username'] = '';
+$db['development']['password'] = '';
+$db['development']['db_debug'] = TRUE;
+$db['development']['stricton'] = FALSE;
+$db['development']['char_set'] = 'utf8';
